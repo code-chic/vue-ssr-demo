9b26bc132f4ec6f81f19ce96f5ebcd680bccd30e
code-chic <codelint@icloud.com>
2020-07-18 22:12:03 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   rules: {
     // 生产环境下禁止使用 console 在控制台输出
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 生产环境下禁止使用 debugger 调试模式
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // 生产环境下禁止使用 alert 警告框

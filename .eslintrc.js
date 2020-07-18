43d463861f3db9270f96b235bff4246125eabc1b
code-chic <codelint@icloud.com>
2020-07-18 18:06:32 +0800

    添加elint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,11 @@ module.exports = {
     'vue'
   ],
   rules: {
+    // 生产环境下禁止使用 console 在控制台输出
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 生产环境下禁止使用 debugger 调试模式
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 生产环境下禁止使用 alert 警告框
+    'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }

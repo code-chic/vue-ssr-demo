150304102fd133b118397cc2be0b3c7491e350a4
code-chic <codelint@icloud.com>
2020-07-18 17:50:20 +0800

    initial create project.

---
+++ app/views/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">这里是关于我们</div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.home {
+  color: blue;
+}
+</style>
---
+++ app/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">这里是首页</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  color: red;
+}
+</style>

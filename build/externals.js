d1f9280608398590727e2650aa87f4942963057f
code-chic <codelint@icloud.com>
2020-07-18 18:49:37 +0800

    添加外置化配置项

---
+++ build/externals.js
@@ -0,0 +1,13 @@
+// 外置化配置项
+exports.externalScriptConfigs = [
+  {
+    package: 'vue',
+    globalNamespace: 'Vue',
+    assetUrl: 'https://cdn.jsdelivr.net/npm/vue'
+  },
+  {
+    package: 'vue-router',
+    globalNamespace: 'VueRouter',
+    assetUrl: 'https://cdn.jsdelivr.net/npm/vue-router@latest'
+  }
+]

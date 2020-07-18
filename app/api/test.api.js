d518e59115016c60c7d9ac1a5adc3ee8b575bcf1
code-chic <codelint@icloud.com>
2020-07-18 22:04:55 +0800

    添加测试接口

---
+++ app/api/test.api.js
@@ -0,0 +1,7 @@
+import * as fetch from '@/util/fetch'
+
+// 获取测试接口数据
+export const getTestData = params => fetch.doGet('/test/getTestData', params)
+
+// 新增测试数据接口
+export const addTestData = params => fetch.doPost('/test/addTestData', params)

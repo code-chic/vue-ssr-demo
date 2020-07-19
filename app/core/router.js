aab21a086d4c652d157c07d0b049ad2bce565199
code-chic <codelint@icloud.com>
2020-07-19 09:38:57 +0800

    删除无用代码

--- app/core/router.js
+++ app/core/router.js
@@ -25,23 +25,3 @@ export function createRouter () {
     }]
   })
 }
-// const router = new VueRouter({
-//   baseURL: '',
-//   mode: 'history',
-//   routes: [{
-//     path: '/',
-//     component: Home,
-//     meta: {
-//       title: '首页',
-//       keepAlive: true
-//     }
-//   }, {
-//     path: '/about',
-//     component: () => import('@/views/about/index'),
-//     meta: {
-//       title: '关于我们'
-//     }
-//   }]
-// })
-//
-// export default router

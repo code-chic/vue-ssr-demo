6facb3711e746fe3406fb678af6a904d56f9a4d1
code-chic <codelint@icloud.com>
2020-07-20 16:04:39 +0800

    删除部分无用代码

--- app/core/main.js
+++ app/core/main.js
@@ -36,26 +36,3 @@ export function createApp () {
     store
   }
 }
-
-// import Vue from 'vue'
-// import { createRouter } from '@/core/router/index'
-// import { createStore } from '@/core/store/index'
-// import { sync } from 'vuex-router-sync'
-// import App from '@/views/App'
-//
-// import '@/style/index.css'
-//
-// export default function initApp () {
-//   // 创建 router 和 store 实例
-//   const router = createRouter()
-//   const store = createStore()
-//   // 同步路由状态(route state)到 store
-//   sync(store, router)
-//
-//   const app = new Vue({
-//     store,
-//     router,
-//     render: h => h(App)
-//   })
-//   return { app, router, store }
-// }
